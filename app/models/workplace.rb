cdf8552f8238bfd94500c45dbaa525204038ba6a
Milan Barande <milan.barande@hotmail.fr>
2017-07-19 10:27:36 -0400

    features added to the seed

--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -1,10 +1,10 @@
 class Workplace < ApplicationRecord
 
   has_many :features, through: :workplacefeatures
-  has_many :visits
-  has_many :workplacephotos
-  has_many :favourites
-  has_many :reviews
+  has_many :visits, dependent: :destroy
+  has_many :workplacephotos, dependent: :destroy
+  has_many :favourites, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :address, presence: true
   validates :category, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts Workplace.destroy_all
+puts "Destroying all the element in the db..."
+Workplace.destroy_all
+Feature.destroy_all
+puts "Done."
+
 
 @client = GooglePlaces::Client.new(ENV['GOOGLE_PLACE_API'])
 
 coworkings = @client.spots_by_query("coworking montreal")
 libraries = @client.spots_by_query("library montral")
 
-
+puts "Seeding coworking places..."
 coworkings.each do |coworking|
-  puts Workplace.create(google_id: coworking.place_id, name: coworking.name, category: 0, address: coworking.formatted_address, longitude: coworking.lng, latitude: coworking.lat)
+  Workplace.create!(google_id: coworking.place_id, name: coworking.name, category: 0, address: coworking.formatted_address, longitude: coworking.lng, latitude: coworking.lat)
 end
+puts "Done."
 
+puts "Seeding libraries..."
 libraries.each do |library|
-  puts Workplace.create(google_id: library.place_id, name: library.name, category: 1, address: library.formatted_address, longitude: library.lng, latitude: library.lat)
+  Workplace.create!(google_id: library.place_id, name: library.name, category: 1, address: library.formatted_address, longitude: library.lng, latitude: library.lat)
 end
+puts "Done."
+
+features = ["wifi", "large tables", "hot drinks", "cold drinks", "snacks", "meals", "electric outlets", "private rooms", "hourly rate" ]
+
+puts "Seeding the features..."
+features.each do |feature|
+  puts Feature.create!(name: feature)
+end
+puts "Done."
 
 # response = RestClient.get "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{coworking.photos.first.photo_reference
 # }&key=#{ENV['GOOGLE_PLACE_API']}"
