f0c52cf046edc6f8aef6ff5267205673a64a1b28
Romain BOBOE <rboboe@gmail.com>
2017-07-19 12:13:11 -0400

    rating collection

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,10 @@ class Rating < ApplicationRecord
   belongs_to :review
 
   DESCRIPTIONS = ["wifi", "noise", "comfort", "service"]
+
+  validates :comment,
+  presence: true,
+  length: {maximum: 480},
+  on: :create,
+  allow_nil: false
 end
