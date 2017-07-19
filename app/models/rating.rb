6d4529365d3b861c4989f722863f983aa7149f73
Romain BOBOE <rboboe@gmail.com>
2017-07-19 13:22:00 -0400

    rating fixeeeeeeeeeeed

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,11 +1,4 @@
 class Rating < ApplicationRecord
   belongs_to :review
 
-  DESCRIPTIONS = ["wifi", "noise", "comfort", "service"]
-
-  validates :comment,
-  presence: true,
-  length: {maximum: 480},
-  on: :create,
-  allow_nil: false
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :workplace
-  has_many :ratings
+
+  has_many :ratings , inverse_of: :review
+  accepts_nested_attributes_for :ratings
+
+  validates :comment,
+  presence: true,
+  length: {maximum: 480},
+  on: :create,
+  allow_nil: false
 end
