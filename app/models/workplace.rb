41bda79a43a8887b021f5e957d8240c379f19041
Milan Barande <milan.barande@hotmail.fr>
2017-07-22 17:46:35 -0400

    geocoded by address

--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -1,7 +1,8 @@
 class Workplace < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
-
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   has_many :features, through: :workplacefeatures
   has_many :workplacefeatures, dependent: :destroy
