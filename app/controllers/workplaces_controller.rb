1e1483fc063c24a502dc316ee636733c13b2dc8d
Jean-Sebastien Dufresne <jsdufresne@iciela.org>
2017-07-25 06:07:38 -0400

    changes

--- app/controllers/workplaces_controller.rb
+++ app/controllers/workplaces_controller.rb
@@ -44,6 +44,7 @@ class WorkplacesController < ApplicationController
     @review = Review.new
     @client = GooglePlaces::Client.new(ENV['GOOGLE_PLACE_API'])
     @spot = @client.spot(@workplace.google_id)
+    @visits = @workplace.visits.where(checkin: true)
 
     @workplace_coordinates = { lat: @workplace.latitude, lng: @workplace.longitude }
     @hash = Gmaps4rails.build_markers(@workplace) do |place, marker|
