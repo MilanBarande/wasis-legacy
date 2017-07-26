ddf4b1b38fd2dbb86508d68584b5ef074dc646e9
Milan Barande <milan.barande@hotmail.fr>
2017-07-26 16:18:20 -0400

    migration done and form updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724214723) do
+ActiveRecord::Schema.define(version: 20170726201029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,10 @@ ActiveRecord::Schema.define(version: 20170724214723) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.text     "bio"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "linkedin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -118,6 +122,7 @@ ActiveRecord::Schema.define(version: 20170724214723) do
     t.string   "google_id"
     t.string   "photo"
     t.float    "global_rating"
+    t.string   "phone_number"
   end
 
   add_foreign_key "favourites", "users"
