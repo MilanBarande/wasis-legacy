c9974ea082c7f6e7d24308de0a6ba8f4d1f124df
Milan Barande <milan.barande@hotmail.fr>
2017-07-28 08:58:14 -0400

    force login to see show

--- app/controllers/workplaces_controller.rb
+++ app/controllers/workplaces_controller.rb
@@ -1,6 +1,6 @@
 class WorkplacesController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   include ApplicationHelper
 
   def new
