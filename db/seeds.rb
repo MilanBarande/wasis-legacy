957d784720f2119dda75cd65a7e2754dc0c6d078
Milan Barande <milan.barande@hotmail.fr>
2017-07-28 11:48:36 -0400

    old filter back on workplace card

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ coworkings.each do |coworking|
   workplace.remote_photo_url = 'http://res.cloudinary.com/dj0wtma0d/image/upload/v1501182470/la_gare_eum2li.jpg'
   workplace.save
   3.times do
-    Workplacefeature.create!(workplace: workplace, feature: Feature.order("RANDOM()").first)
+    Workplacefeature.create!(workplace: workplace, feature: Feature.order("RANDOM()").first) unless workplace == Workplace.find_by_name('La Gare')
   end
 end
 puts "Done."
@@ -49,6 +49,26 @@ coffees.each do |coffee|
   end
 end
 puts "Done"
+
+puts "Seeding custom workplaces..."
+puts "Caravane in creation..."
+caravane = Workplace.create!(name: 'Caravane Café', category: 2, address: '3506 Avenue Lacombe, Montréal, QC H3T 1X5')
+caravane.remote_photo_url = 'http://res.cloudinary.com/dj0wtma0d/image/upload/v1500909503/sdyhnqe0bjsx3aqo2oli.jpg'
+caravane_features = [{name: "Wifi", icon: "feature_icon_2.png"}, {name: "Large tables", icon:"feature_icon_4.png"}, {name:"Hot drinks", icon:"feature_icon_1.png"}, {name:"Cold drinks", icon:"feature_icon_3.png"}, {name:"Snacks", icon:"feature_icon_7.png"}, {name:"Meals", icon:"feature_icon_6.png"}, {name:"Electric outlets", icon:"feature_icon_5.png"} ]
+caravane.save
+caravane_features.each do |feature|
+  Workplacefeature.create!(workplace: caravane, feature: Feature.where(name: feature[:name]).first)
+end
+puts "Caravane created."
+
+puts "La gare update..."
+la_gare = Workplace.find_by_name('La Gare')
+la_gare_features = [{name: "Wifi", icon: "feature_icon_2.png"}, {name: "Large tables", icon:"feature_icon_4.png"}, {name:"Hot drinks", icon:"feature_icon_1.png"}, {name:"Electric outlets", icon:"feature_icon_5.png"}, {name:"Private rooms", icon:"feature_icon_8.png"}, {name:"Hourly rate", icon:"feature_icon_9.png"} ]
+la_gare_features.each do |feature|
+  Workplacefeature.create!(workplace: la_gare, feature: Feature.where(name: feature[:name]).first)
+end
+
+puts "DONE."
 
 # response = RestClient.get "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{coworking.photos.first.photo_reference
 # }&key=#{ENV['GOOGLE_PLACE_API']}"
